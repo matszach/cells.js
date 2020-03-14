a8330117ced3e60e22064f90165ba7910a4b0645
matszach <lkaszubowski@gmail.com>
2020-03-14 15:04:15 +0100

    food color change

--- js/src/food-factory.js
+++ js/src/food-factory.js
@@ -3,7 +3,7 @@ class FoodFactory extends _EntityFactory {
     createRandom() {
         let pos = this.positionGen.next().value;
         let r = Gmt.randInt(0, 150);
-        let g = Gmt.randInt(150, 200);
+        let g = Gmt.randInt(200, 255);
         let b = Gmt.randInt(0, 100);
         let val = Gmt.randInt(St.Food.minInitialValue, St.Food.maxInitialValue);
         return new Food(pos, r, g, b, val);
